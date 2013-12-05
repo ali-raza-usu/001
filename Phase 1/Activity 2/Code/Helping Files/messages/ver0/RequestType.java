4ab1955c752bbb6beca6b96fa070222f003afa9d
Ali Raza <ali.raza@aggiemail.usu.edu>
2013-12-04 21:12:13 -0700

    CHAE

--- Phase 1/Activity 2/Code/Helping Files/messages/ver0/RequestType.java
+++
@@ -1,6 +0,0 @@
-package exp.qos.messages.ver0;
-
-public enum RequestType {
-
-	SEND, PAUSE, STOP;
-}
--- Phase 3/Phase 2 changes/coreApps.WeatherSimulator.extended/src/utilities/messages/ver0/RequestType.java
+++
@@ -1,6 +0,0 @@
-package utilities.messages.ver0;
-
-public enum RequestType {
-
-	SEND, PAUSE, STOP;
-}
